0d6d9615c6c0e927992edafcc61aae34f89247ca
Alan Russo <alanrussobmp@gmail.com>
2017-01-13 00:54:33 -0200

    criando as primeiras rotas

--- config/express.js
+++ config/express.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 
+
+var home = require('./../app/routes/home');
+
+home(app);
 app.set('port',3000);
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
 
 app.use(express.static('./public'));
 
