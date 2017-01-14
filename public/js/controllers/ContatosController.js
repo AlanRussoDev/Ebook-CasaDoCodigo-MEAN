f70a24a9b485a81d213603c11ce67b8d6e03dab3
Alan Russo <alanrussobmp@gmail.com>
2017-01-14 15:11:19 -0200

    adicionando estilo as paginas com bootstrap e ngRepeat

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,5 +1,11 @@
 angular.module('contatooh').controller('ContatosController', function ($scope) {
 
+    $scope.contatos = [
+    {_id: "1",nome: 'Contato Exemplo 1', email : 'cont1@empresa.com.br'},
+    {_id: "2",nome: 'Contato Exemplo 2', email : 'cont2@empresa.com.br'},
+    {_id: "3",nome: 'Contato Exemplo 3', email : 'cont3@empresa.com.br'}
+]
+
     $scope.total = 0;
 
     $scope.incrementa = function () {
