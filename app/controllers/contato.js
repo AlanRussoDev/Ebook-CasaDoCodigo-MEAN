103a4cebcdf788f413d499ce23e474464098c73d
Alan Russo <alanrussobmp@gmail.com>
2017-01-15 15:17:49 -0200

    colocando todas as rotas para funcionar na lista de contatos

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,7 +1,7 @@
-var contatos = [
-    {_id: "1",nome: 'Contato Exemplo 1'},
-    {_id: "2",nome: 'Contato Exemplo 2'},
-    {_id: "3",nome: 'Contato Exemplo 3'}
+ var contatos = [
+    {_id: "1",nome: 'Contato Exemplo 1', email : 'cont1@empresa.com.br'},
+    {_id: "2",nome: 'Contato Exemplo 2', email : 'cont2@empresa.com.br'},
+    {_id: "3",nome: 'Contato Exemplo 3', email : 'cont3@empresa.com.br'}
 ]
 
 module.exports = function(){
@@ -19,6 +19,20 @@ module.exports = function(){
 
         contato ? res.json(contato) : res.status(404).send('Contato não encontrado');
         console.log(req.params.id);
+    }
+
+    controller.removeContato = function(req, res){
+
+        var idContato = req.params.id;
+        contatos = contatos.filter(function(contato){
+            return contato._id != idContato;
+        })
+
+        res.send(204).end();
+    }
+
+    controller.salvaContato = function(req, res){
+
     }
 
     return controller;
--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,14 +1,62 @@
-angular.module('contatooh').controller('ContatosController', function ($scope) {
+angular.module('contatooh').controller('ContatosController', function ($scope, $resource) {
+     // $scope.contatos = [
+    //     {_id: "1",nome: 'Contato Exemplo 1', email : 'cont1@empresa.com.br'},
+    //     {_id: "2",nome: 'Contato Exemplo 2', email : 'cont2@empresa.com.br'},
+    //     {_id: "3",nome: 'Contato Exemplo 3', email : 'cont3@empresa.com.br'}
+    // ]
 
-    $scope.contatos = [
-    {_id: "1",nome: 'Contato Exemplo 1', email : 'cont1@empresa.com.br'},
-    {_id: "2",nome: 'Contato Exemplo 2', email : 'cont2@empresa.com.br'},
-    {_id: "3",nome: 'Contato Exemplo 3', email : 'cont3@empresa.com.br'}
-]
+    var init = function(){
+        $scope.mensagem = {texto : ''};
+        $scope.contatos = [];
+        $scope.filtro = '';        
+        buscaContatos();        
+    };
 
-    $scope.total = 0;
+    init();
 
-    $scope.incrementa = function () {
-        $scope.total++;
+    function buscaContatos(){
+        
+        var Contato = $resource('/contatos');
+        Contato.query(function(contatos){
+                $scope.contatos = contatos; 
+            }, function(error){
+                $scope.mensagem = {
+                    texto : 'Não foi possível obter a lista de contatos'
+                };
+                console.log(error)
+        })
     }
+
+   
+   $scope.remove = function(contato){
+       var Contato = $resource('/contatos/:id');
+       Contato.delete({id : contato._id},
+        buscaContatos,
+        function(erro){
+            $scope.mensagem = {
+                    texto : 'Não foi possivel remover o contato'
+                };
+            console.log(erro)
+        }
+       );
+   }
+
+    // $scope.total = 0;
+
+    // $scope.incrementa = function () {
+    //     $scope.total++;
+    // }
+
+    // $http.get('/contatos')
+    //     .success(function(data){
+    //         $scope.contatos = data;
+    //         // $scope.total = data.length;
+    //     })
+    //     .error(function(error){
+    //         console.log('Não foi possível obter a lista de contatos')
+    //         console.log(error)
+    //     })
+
+  
+    
 });
