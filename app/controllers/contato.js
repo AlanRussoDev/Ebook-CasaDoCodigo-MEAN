d9fe9149c30fb119537f92752b75f855d6790048
Alan Russo <alanrussobmp@gmail.com>
2017-01-16 08:56:51 -0200

    commit depois do final de semana

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,7 +1,7 @@
  var contatos = [
-    {_id: "1",nome: 'Contato Exemplo 1', email : 'cont1@empresa.com.br'},
-    {_id: "2",nome: 'Contato Exemplo 2', email : 'cont2@empresa.com.br'},
-    {_id: "3",nome: 'Contato Exemplo 3', email : 'cont3@empresa.com.br'}
+    {nome: 'Contato Exemplo 1', email : 'cont1@empresa.com.br'},
+    {nome: 'Contato Exemplo 2', email : 'cont2@empresa.com.br'},
+    {nome: 'Contato Exemplo 3', email : 'cont3@empresa.com.br'}
 ]
 
 var ID_CONTATO_INC = 3;
